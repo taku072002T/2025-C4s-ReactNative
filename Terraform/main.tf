bf34f730020e5e22efe5a37241b143f4061a2a4f
taku072002T <taku07200@gmail.com>
2025-03-07 17:47:35 +0900

    Changed the states of terraforms

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -22,7 +22,7 @@ resource "google_project" "default" {
 
   # project_id は全世界で一意になる必要がある。
   # 今回は、各自で適当にidを変更すること。しかし、大文字は使えない。
-  project_id      = "c4s-cloudff-sandbox-20250304"
+  project_id      = "c4s-cloudfff-sandbox-20250304"
   # これはなんでもいい。
   name            = "C4s-CloudF-sandbox"
 
@@ -64,32 +64,12 @@ resource "google_firebase_project" "default" {
 resource "google_firebase_web_app" "default" {
   provider     = google-beta
   project      = google_project.default.project_id
-  display_name = "C4s-CloudF-sandbox Web App"
+  display_name = "Todoアプリ"
   depends_on   = [google_firebase_project.default]
 }
 
-# Firebase Web SDKの設定ファイルを取得
-resource "google_firebase_web_app_config" "default" {
-  provider   = google-beta
-  project    = google_project.default.project_id
-  web_app_id = google_firebase_web_app.default.app_id
-}
-
-output "firebase_config" {
-  value = {
-    api_key            = google_firebase_web_app.default.api_key
-    auth_domain        = "${google_project.default.project_id}.firebaseapp.com"
-    database_url       = "https://${google_project.default.project_id}-default-rtdb.asia-southeast1.firebasedatabase.app"
-    project_id         = google_project.default.project_id
-    storage_bucket     = "${google_project.default.project_id}.appspot.com"
-    messaging_sender_id = google_firebase_web_app.default.messaging_sender_id
-    app_id             = google_firebase_web_app.default.app_id
-  }
-  sensitive = true
-}
-
-# Firebase Web SDKの設定ファイルを取得
-resource "google_firebase_web_app_config" "default" {
+# Firebase Web SDKの設定ファイル取得（リソースではなくデータソースとして使用）
+data "google_firebase_web_app_config" "default" {
   provider   = google-beta
   project    = google_project.default.project_id
   web_app_id = google_firebase_web_app.default.app_id
@@ -98,23 +78,26 @@ resource "google_firebase_web_app_config" "default" {
 # 環境変数ファイル用の変数を設定
 locals {
   env_content = <<-EOT
-    FIREBASE_API_KEY=${google_firebase_web_app_config.default.api_key}
+    FIREBASE_API_KEY=${data.google_firebase_web_app_config.default.api_key}
     FIREBASE_AUTH_DOMAIN=${google_project.default.project_id}.firebaseapp.com
     FIREBASE_DATABASE_URL=https://${google_project.default.project_id}-default-rtdb.asia-southeast1.firebasedatabase.app
     FIREBASE_PROJECT_ID=${google_project.default.project_id}
     FIREBASE_STORAGE_BUCKET=${google_project.default.project_id}.appspot.com
-    FIREBASE_MESSAGING_SENDER_ID=${google_firebase_web_app_config.default.messaging_sender_id}
+    FIREBASE_MESSAGING_SENDER_ID=${data.google_firebase_web_app_config.default.messaging_sender_id}
     FIREBASE_APP_ID=${google_firebase_web_app.default.app_id}
   EOT
+}
 
-  google_services_json = jsonencode({
-    "apiKey"            = google_firebase_web_app_config.default.api_key
-    "authDomain"        = "${google_project.default.project_id}.firebaseapp.com"
-    "databaseURL"       = "https://${google_project.default.project_id}-default-rtdb.asia-southeast1.firebasedatabase.app"
-    "projectId"         = google_project.default.project_id
-    "storageBucket"     = "${google_project.default.project_id}.appspot.com"
-    "messagingSenderId" = google_firebase_web_app_config.default.messaging_sender_id
-    "appId"             = google_firebase_web_app.default.app_id
+# 設定ファイルをJSONとしても出力
+locals {
+  firebase_config_json = jsonencode({
+    apiKey            = data.google_firebase_web_app_config.default.api_key
+    authDomain        = "${google_project.default.project_id}.firebaseapp.com"
+    databaseURL       = "https://${google_project.default.project_id}-default-rtdb.asia-southeast1.firebasedatabase.app"
+    projectId         = google_project.default.project_id
+    storageBucket     = "${google_project.default.project_id}.appspot.com"
+    messagingSenderId = data.google_firebase_web_app_config.default.messaging_sender_id
+    appId             = google_firebase_web_app.default.app_id
   })
 }
 
@@ -124,24 +107,16 @@ resource "local_file" "env_file" {
   filename = "${path.module}/.env"
   
   depends_on = [
-    google_firebase_web_app_config.default
+    data.google_firebase_web_app_config.default
   ]
 }
 
-# google-services.jsonファイル自動生成
-resource "local_file" "google_services_json" {
-  content  = local.google_services_json
-  filename = "${path.module}/google-services.json"
+# firebaseConfig.jsonファイルも生成
+resource "local_file" "firebase_config_json" {
+  content  = local.firebase_config_json
+  filename = "${path.module}/firebaseConfig.json"
   
   depends_on = [
-    google_firebase_web_app_config.default
+    data.google_firebase_web_app_config.default
   ]
-}
-
-# Firebase Webアプリの作成
-resource "google_firebase_web_app" "default" {
-  provider     = google-beta
-  project      = google_project.default.project_id
-  display_name = "C4s-CloudF-sandbox Web App"
-  depends_on   = [google_firebase_project.default]
 }
